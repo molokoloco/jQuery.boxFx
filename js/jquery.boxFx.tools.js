f510378b82f453973aa6ea040ba47ca68b1f3cdb
Julien G <molokoloco@gmail.com>
2012-05-22 17:40:28 +0300

    Update js/jquery.boxFx.tools.js

--- js/jquery.boxFx.tools.js
+++ js/jquery.boxFx.tools.js
@@ -1,13 +1,13 @@
 /*
     // jQuery tools
     // GPL/MIT License - @molokoloco 2011 - http://b2bweb.fr
-    // Dependancy to ./moderniz.js for $.fn.crossCss
+    // Dependancy to moderniz.js for $.fn.crossCss
 */
 
 // I use this for debuging ^^
 var db  = function() { 'console' in window && console.log.call(console, arguments); };
 
-(function ($, Modernizr, window) {
+(function($, window) {
 
     $.toolsLoaded = true; // Declare tools.js as loaded...
 
@@ -77,10 +77,10 @@ var db  = function() { 'console' in window && console.log.call(console, argument
         return parseInt(miin + (Math.random() * (maax - miin)), 10);
     };
 
-    // Lightest TPL // $.getTpl('<div>{title}</div>', {title:'toto'}) // '<div>toto</div>'
+    // Lightest TPL // $.getTpl('<div>#title#</div>', {title:'toto'}) // '<div>toto</div>'
     $.getTpl = function(tpl, val) {
         for (var p in val)
-            tpl = tpl.replace(new RegExp('({'+p+'})', 'g'), val[p] || '');
+            tpl = tpl.replace(new RegExp('#'+p+'#', 'gi'), val[p] || '');
         return tpl;
     };
 
@@ -264,4 +264,4 @@ var db  = function() { 'console' in window && console.log.call(console, argument
         return {animation:cssAnimations.join(','), animationFillMode: cssAnimationsFillMode.join(',')}; // apply this to element, with 'crossCss()', to trigger keyframe
     };
 
-})(jQuery, Modernizr, window);
+})(jQuery, window);
